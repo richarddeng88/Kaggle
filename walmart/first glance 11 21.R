a5854b48c20c64c0e77a1629a8ccedd61a090814
Richard <richarddeng88@gmail.com>
2015-12-13 14:06:07 -0500

    12 13 noon

--- walmart/first glance 11 21.R
+++ walmart/first glance 11 21.R
@@ -2,12 +2,14 @@ train <- read.csv("data/walmart/train.csv")
 test <- read.csv("data/walmart/test.csv")
 sample <- read.csv("data/walmart/sample_submission.csv")
 
-sapply(train, function(x){sum(is.na(x))})
-table(train$TripType)
-
-## classification problem.   
-# a lot of NAs, so few predictors and quite a lot of types. 
-# catigoritize the departmentDescription use tree, CV, SVM, and 
+sapply(train, function(x){sum(is.na(x))}) ## check NAs
+sapply(test, function(x){sum(is.na(x))}) ## check NAs
+prop.table(table(train$TripType)) 
+barplot(table(train$TripType))              # barplot response var
+barplot(prop.table(table(train$TripType)))  # barplot response var
+## classification problem : TREE, RF, QDA, 
+## two variables have 4129 NAS; Response var looks like emblanced. 
+# 
 
 
 
