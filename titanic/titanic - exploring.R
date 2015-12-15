9682867798ae2a3c309a2c9ba20fd8210f0f511c
Richard <richarddeng88@gmail.com>
2015-12-14 23:18:19 -0500

    12 14 night

--- titanic/titanic - exploring.R
+++ titanic/titanic - exploring.R
@@ -1,4 +1,4 @@
-library(caret)
+library(caret);library(randomForest);library(rpart.plot)
 train <- all_data
 
 table(train$Pclass)
@@ -16,13 +16,23 @@ training %>% group_by(Pclass) %>% summarize(mean(Survived))
 b <- training %>% group_by(Age) %>% summarize(mean(Survived))
 
 rpart.plot(tree_model)
-varImpPlot(rf_model)
+varImpPlot(rf_model$finalModel)
 
 # train RF model using "caret" package
     grid <- expand.grid(.mtry=c(2,3,4,5,6,7))
-    rf_model <- train(Survived~.,
+    rf_model <- train(as.factor(Survived)~Pclass + Sex + Age + SibSp + Parch + Fare + Embarked,
                       data=training,
                       importance=T,
+                      tuneGrid =grid,
                       method="rf")
     rf_pred <- predict(rf_model, test)
-    
+    solution <- data.frame(PassengerID=test$PassengerId, Survived=rf_pred)
+    write.csv(solution,file="kaggle/titanic/submission.csv",row.names = F)
+
+# train QDA MODEL
+    qda_model <- train(as.factor(Survived)~Pclass + Sex + Age + SibSp + Parch + Fare + Embarked,
+                       data=training,
+                       method="qda")
+    qda_pred <- predict(qda_model, test)
+    solution <- data.frame(PassengerID=test$PassengerId, Survived=qda_pred)
+    write.csv(solution,file="kaggle/titanic/submission.csv",row.names = F)
