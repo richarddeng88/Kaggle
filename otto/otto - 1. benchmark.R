1726aaa2c85b05b0007fdd42aa7239ff3431de70
Richard <richarddeng88@gmail.com>
2015-12-27 12:43:14 -0500

    12 27 morn

--- otto/otto - 1. benchmark.R
+++ otto/otto - 1. benchmark.R
@@ -8,7 +8,27 @@ prop.table(table(train$target))
 
 submission <- data.frame(id=test$id, Class_1=NA, Class_2=NA, Class_3=NA, Class_4=NA, Class_5=NA, Class_6=NA, Class_7=NA, Class_8=NA, Class_9=NA)
 
-rf <- randomForest(train$target, train[,c(-1,-95)], ntree=500, importance=TRUE)
-varImpPlot(rf)
+# TRAIN RF MODEL  -  BENCHMARKE
+        rf <- randomForest(train$target, train[,c(-1,-95)], ntree=500, importance=TRUE)
+        varImpPlot(rf)
+        importance(rf)
+        imp <- importance(rf, type=1)
+        featureImportance <- data.frame(Feature=row.names(imp), Importance=imp[,1])
+        # PREDICTION AND OUTPUT
+        submission[,2:10] <- (predict(rf, test[,-1], type="prob")+0.01)/1.09
+        rf_prob <- predict(rf, test[,-1], type="prob")
+        # submission[,2:10] <- predict(rf, test[,-1], type="prob")
+        write.csv(submission, file="submission.csv",row.names = F)
 
-imp <- importance(rf, type=1)
+# ploting the important features
+        ggplot(featureImportance, aes(x=reorder(Feature, Importance), y=Importance)) +
+                geom_bar(stat="identity", fill="#53cfff") +
+                coord_flip() + 
+                theme_light(base_size=20) +
+                xlab("Importance") +
+                ylab("") + 
+                ggtitle("Random Forest Feature Importance\n") +
+                theme(plot.title=element_text(size=18))
+
+        ggsave("feature_importance.png", p, height=20, width=8, units="in")
+        
