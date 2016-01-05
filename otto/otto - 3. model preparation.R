d5c9c471ce0fa3c5608e5cd346964215ce0f2558
Richard <richarddeng88@gmail.com>
2016-01-05 18:21:17 -0500

    01 05 noble

--- otto/otto - 3. model preparation.R
+++ otto/otto - 3. model preparation.R
@@ -2,6 +2,7 @@ library(caret); library(class)
 
 ## now, we have training and validation set. 
         # RF
+        
         rf_model <- randomForest(target~., 
                                  data= training[,-1], 
                                  ntree=100, 
@@ -10,8 +11,17 @@ library(caret); library(class)
         rf_pred <- predict(rf_model, validation[,-1])
         confusionMatrix(rf_pred, validation$target) # 0.8062
         
-
+        # naive bayes
+        
+        
         # KNN
+        # ormalize <- function(x) { return ((x - mean(x)) / sd(x))}
+        # sapply(training[,2:94], function(x){mean(x)})
+        stand <- preProcess(training[,2:94], method = c("center","scale"))
+        knn_training <- predict(stand, training)
+        knn_validation <- predict(stand, validation)
+        
+        
         knn_model <- knn(training[,c(-1,-95)], 
                          validation[, c(-1,-95)],
                          training$target,
@@ -28,6 +38,11 @@ library(caret); library(class)
         lda_pred <- predict(lda_model, validation[,-1])
         confusionMatrix(lda_pred, validation$target) #0.7041
         
-        #knn_model <- train(target~.,
-        #                   data=training[,-1],
-        #                   method= "knn")
+        
+        # QDA
+        qda_model <- train(target~., 
+                           data= training[,-1], 
+                           method="qda")
+        qda_pred <- predict(qda_model, validation[,-1])
+        confusionMatrix(qda_pred, validation$target) #0.6673
+        
