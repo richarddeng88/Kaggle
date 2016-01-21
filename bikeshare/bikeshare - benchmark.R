b0b39ac84f7722abdba42e906fd69687cb41f869
Richard <richarddeng88@gmail.com>
2016-01-20 23:02:35 -0500

    01 20 night

--- bikeshare/bikeshare - benchmark.R
+++ bikeshare/bikeshare - benchmark.R
@@ -1,15 +1,72 @@
 library(ggplot2)
 library(lubridate)
 library(randomForest)
+library(dplyr)
 train <- read.csv("data/bike/train.csv")
 test <- read.csv("data/bike/test.csv")
 sample <- read.csv("data/bike/sample.csv")
 
 # 1st glance
-    
+        # HEATMAP - THIS ONE LOOKS GOOD
+        library(lattice)
+        myheatmap <- function(myMAT,low,high,uselabel){
+                colorFun <- colorRampPalette(c("blue","white","red")) 
+                
+                b <- boxplot(myMAT, plot = FALSE)
+                thr <- c(low,high)
+                colbins <- 100
+                step <- abs(thr[2] - thr[1])/50
+                
+                myAT <- seq(thr[1], thr[2], step)
+                
+                myCOLregions <- colorFun(length(myAT))
+                
+                levelplot(myMAT, at = myAT, col.regions = myCOLregions, 
+                          scales=list(x=list(rot=90)), labels=uselabel)
+        }
+        
+        correlation <- cor(train[-1])
+        myheatmap(correlation,-1,1,F)
 
+        # transfor date to new variables. 
+        train$datetime <- ymd_hms(train$datetime)
+        train$weekday <- as.factor(weekdays(train$datetime))
+        train$year <- as.factor(year(train$datetime))
+        train$month <- as.factor(month(train$datetime))
+        train$hour <- as.factor(hour(train$datetime))
+        train <- train[,-1]
+        str(train)
+        
+        test$datetime <- ymd_hms(test$datetime)
+        test$weekday <- as.factor(weekdays(test$datetime))
+        test$year <- as.factor(year(test$datetime))
+        test$month <- as.factor(month(test$datetime))
+        test$hour <- as.factor(hour(test$datetime))
+        str(test)
+        
+        # ploting
+        a <- train %>% group_by(weekday) %>% summarize(mean=mean(count))
+        ggplot(data=a, mapping = aes(x=weekday, y=mean)) + geom_line()
+        plot(train$weekday, train$count)
+        
+        b1 <- train %>% group_by(hour) %>% summarize(mean=mean(count), type="count")
+        b2 <- train %>% group_by(hour) %>% summarize(mean=mean(registered), type="registered")
+        b3 <- train %>% group_by(hour) %>% summarize(mean=mean(casual), type="casual")
+        b <- rbind(b1,b2,b3)
+        ggplot(data=b, mapping = aes(x=hour, y=mean,color=type)) + geom_line()+labs(y="number of users", title="mean values")
 
-# benchmark
+# benchmark -1
+        rf_model <- randomForest(count~.,
+                                 data = train[,c(-9,-10)],
+                                 ntree=500)
+        varImpPlot(rf_model)
+        importance(rf_model)
+        rf_pred <- predict(rf_model, test)
+        submission <- data.frame(datetime=test$datetime, count=rf_pred)
+        write.csv(submission, file = "submission.csv", row.names=FALSE)
+        # score is 0.64535. it seems that i still need to consider the time factor. 
+
+# benchmark -2 
 extractFeatures <- function(data) {
     features <- c("season",
                   "holiday",
@@ -60,3 +117,17 @@ extractFeatures <- function(data) {
         theme(plot.title=element_text(size=18))
     
     ggsave("2_feature_importance.png", p)
+    
+# benchmark -3
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
